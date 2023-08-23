014b353c38a2302fbf14ce0c5b18f7808ed526c7
Buhaiov-A <artyombugayov@gmail.com>
2023-08-23 16:55:48 +0200

    update index.jsx

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,19 +1,19 @@
-import React, { useEffect } from "react";
-import "./style.css";
-import { Route, Routes } from "react-router-dom";
-import Header from "../Header";
-import Footer from "../Footer";
-import MainPage from "../../pages/MainPage";
-import NotFoundPage from "../../pages/NotFoundPage";
-import { useDispatch } from "react-redux";
-import ProductsPage from "../../pages/ProductsPage";
-import CategoriesPage from "../../pages/CategoriesPage";
-import BasketPage from "../../pages/BasketPage";
-import SingleProductPage from "../../pages/SingleProductPage";
-import { fetchCategories } from "../../store/slices/cateroriesSlice";
-import { fetchProducts } from "../../store/slices/productsSlice";
-import "react-toastify/dist/ReactToastify.css";
-import { ToastContainer } from "react-toastify";
+import React, { useEffect } from 'react';
+import './style.css';
+import { Route, Routes } from 'react-router-dom';
+import Header from '../Header';
+import Footer from '../Footer';
+import MainPage from '../../pages/MainPage';
+import NotFoundPage from '../../pages/NotFoundPage';
+import { useDispatch } from 'react-redux';
+import ProductsPage from '../../pages/ProductsPage';
+import CategoriesPage from '../../pages/CategoriesPage';
+import BasketPage from '../../pages/BasketPage';
+import SingleProductPage from '../../pages/SingleProductPage';
+import { fetchCategories } from '../../store/slices/cateroriesSlice';
+import { fetchProducts } from '../../store/slices/productsSlice';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   const dispatch = useDispatch();
@@ -27,7 +27,7 @@ function App() {
     <div>
       <Header />
       <Routes>
-        <Route path="/" element={<MainPage />} />
+        <Route path="/garden-store" element={<MainPage />} />
         <Route path="/categories" element={<CategoriesPage />} />
         <Route
           path="/categories/:categoryTitle/:categoryID"
